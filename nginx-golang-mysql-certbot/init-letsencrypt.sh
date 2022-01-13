6a77bf425205790a3d37cc858124f8c0e542c6bd
Nhat TONG <nhat.tong@sword-group.com>
2022-01-13 10:11:56 +0100

    update script

--- nginx-golang-mysql-certbot/init-letsencrypt.sh
+++ nginx-golang-mysql-certbot/init-letsencrypt.sh
@@ -5,11 +5,17 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(app-35-202-151-31.nip.io)
+echo "Enter your domain: for ex: (app-34-71-48-21.nip.io)"
+read domains
+
+echo "Enter your data path: for ex: ./certbot"
+read data_path
+
+#domains=(app-35-202-151-31.nip.io)
+#data_path="./certbot"
 rsa_key_size=4096
-data_path="./certbot"
 email="" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
@@ -38,8 +44,8 @@ docker-compose run --rm --entrypoint "\
 echo
 
 
-echo "### Starting nginx ..."
-docker-compose up --force-recreate -d nginx
+echo "### Starting reverse proxy ..."
+docker-compose up --force-recreate -d proxy
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
@@ -76,5 +82,5 @@ docker-compose run --rm --entrypoint "\
     --force-renewal" certbot
 echo
 
-echo "### Reloading nginx ..."
-docker-compose exec nginx nginx -s reload
+echo "### Reloading reverse proxy ..."
+docker-compose exec proxy nginx -s reload
